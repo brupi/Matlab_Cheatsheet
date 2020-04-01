3914431201f44024687b44af20526fc86409b45a
Jan Brupbacher <jan.brupbacher@hsr.ch>
2020-04-01 16:56:33 +0200

    added bode and nyquist section and changed design of toc

--- code/transfer_function.m
+++ code/transfer_function.m
@@ -1,7 +1,10 @@
-%% Transfer function
 sys1 = tf(2*[1 1], poly([0 -2]))		% sys1 = 2*(s+1)/((s+1)*s)
 sys2 = tf(poly([-1 -2]), poly([0 -1]))	% poly macht aus Nullstellen ein 
 										% Polynom
 sys = minreal(feedback(sys1, sys2, -1))	% Feedbackschaltung mit feedback
 stability = isstable(sys)				% minreal => loescht kritische 
 										% Polstellen ???
+step(sys1)								% Sprungantwort des Systems
+step(sys1, 2)							% Sprungantwort der ersten 2 sek
+zpk(sys1)								% Faktorisieren der Polynome
+										% (umkehrung von tf())
--- code/tricks.m
+++ code/tricks.m
@@ -1,4 +1,3 @@
-%% Einige kleine Kniffs
 A = B'		% ' bedeutet transponiert
 v = u.*u	% . bedeutet Elementweise Operation -> v ist vektor
 w = u'*u	% w ist skalar (skalarprodukt von u und u)
